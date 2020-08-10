4189daf18731f3425847c00e36ed087f37f64b69
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-08-10 14:29:37 +0100

    Added number checking function examples

--- functions.js
+++ functions.js
@@ -1,8 +1,31 @@
 hello(); // hoisted
 
+let result = add(10, 10);
+let resultAlt = add(10, 20);
+let resultNull = add("hello", 10);
+console.log(result); // 20
+console.log(resultAlt); // 30
+console.log(resultNull); // null
+
 function hello() {
     // Normal function
     console.log("Hello");
+}
+
+function add(num1, num2) {
+    if (typeof num1 == 'number' && typeof num2 == 'number') {
+        return num1 + num2;
+    } else {
+        return null;
+    }
+}
+
+function addAlt(num1, num2) {
+    if (isNaN(num1) || isNaN(num2)) {
+        return null;
+    } else {
+        return num1 + num2;
+    }
 }
 
 // Arrow function, cannot be hoisted
